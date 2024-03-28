c69d80fc2b9b00d0545e2682eb7f12b6ec575db0
FelipeAmorimDEV <felipecastroo.ads@gmail.com>
2024-03-28 12:44:37 -0300

    ✔  Refatoração: Componentes separados por arquivo

---
+++ src/components/movies.jsx
@@ -0,0 +1,17 @@
+import { getMoviePoster } from "../utils/get-movie-poster"
+
+const Movies = ({ movies, onClickMovie }) => {
+  return (
+    movies.map((movie) => (
+      <li key={movie.id} onClick={() => onClickMovie(movie)}>
+        <img src={getMoviePoster(movie.poster)} alt={`Poster de ${movie.title}`} />
+        <h3>{movie.title}</h3>
+        <p>
+          <span>📅</span>
+          <span>{movie.year}</span>
+        </p>
+      </li>
+    )))
+}
+
+export { Movies }
